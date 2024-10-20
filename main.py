766660038ee34033aa44974c73c9876fea8c05cf
Marissa Aragon <marag27@wgu.edu>
2024-10-20 15:25:43 -0600

    Added more detailed output for all packages

--- main.py
+++ main.py
@@ -35,6 +35,18 @@ def get_distance(address1, address2):
 
     return float(distance)
 
+# Function to get correct address for package 9
+def update_address(event_time):
+    event_time = datetime.strptime(event_time, "%H:%M").time()
+    if datetime.now().time() >= event_time:  # Ensure the time has reached 10:20
+        package = package_hash.get(9)  # Get package #9
+        new_address_id = get_address("410 S State St")
+        if new_address_id:
+            package.address = "410 S State St" # Update address
+            package.address_id = new_address_id
+        else:
+            print("Failed to update address for package #9. Address not found.")
+
 # Creating 3 trucks, loading them with packages, set starting address, set depart time
 truck1 = Truck(16, 18, None, [1, 13, 14, 15, 16, 19, 20, 29, 30, 31, 34, 37, 40], 0.0, "4001 South 700 East", time(8,0))
 truck2 = Truck(16, 18, None, [3, 6, 9, 12, 17, 18, 21, 22, 23, 24, 26, 27, 35, 36, 38, 39], 0.0, "4001 South 700 East", time(10,20))
@@ -86,23 +98,25 @@ def nearest_neighbor(truck):
 
 # Call nearest_neighbor algorithm for each truck
 nearest_neighbor(truck1)
-nearest_neighbor(truck2)
 nearest_neighbor(truck3)
+update_address("10:20")
+nearest_neighbor(truck2)
 
 # Function to get status for all packages in each truck
 def get_status_all(user_time):
     user_time = datetime.strptime(user_time, "%H:%M").time()
-    print(f"Checking status at {user_time}:")
+    print(f"Checking all package statuses at {user_time}:")
     for i, truck in enumerate([truck1, truck2, truck3], start=1):
         print(f"\nTruck {i}:")
         for package_id in truck.packages:
             package = package_hash.get(package_id)
+            print(f"Package ID: {package.package_id}, Address: {package.address}, Deadline: {package.deadline}, Truck: {i}")
             if package.delivery_time and (datetime.combine(datetime.today(), time(0, 0)) + package.delivery_time).time() <= user_time:
-                print(f"Package {package.package_id} was delivered at {(datetime.combine(datetime.today(), time(0, 0)) + package.delivery_time).time()}.")
+                print(f"  Status: Delivered at {(datetime.combine(datetime.today(), time(0, 0)) + package.delivery_time).time()}")
             elif package.departure_time and package.departure_time <= user_time:
-                print(f"Package {package.package_id} is en route (departed at {package.departure_time}).")
+                print(f"  Status: En Route (departed at {package.departure_time})")
             else:
-                print(f"Package {package.package_id} is at the hub (not yet departed).")
+                print("  Status: At Hub (not yet departed)")
 
 # Function to get status of a specific package that a user chooses
 def get_status_package(package_id, user_time):
